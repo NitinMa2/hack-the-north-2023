e986ccb1a5f773d81c35074e98cabeedee57acc0
NitinMa2 <nitinmathew.official@gmail.com>
2023-09-17 16:04:17 +0800

    bundling code for easy integration

--- audio-manipulation/combine_audio.py
+++ audio-manipulation/combine_audio.py
@@ -1,7 +1,12 @@
 from moviepy.editor import VideoFileClip, AudioFileClip, concatenate_videoclips, CompositeAudioClip
 
-sound1 = AudioFileClip("mixkit-retro-game-emergency-alarm-1000.wav")
-sound2 = AudioFileClip("mixkit-crickets-and-insects-in-the-wild-ambience-39.wav")
 
-mixed = CompositeAudioClip([sound1, sound2.set_start(2)])
-mixed.write_audiofile("mixed-output.mp3", fps =16000)
+def combine_audio(sound_file_1, sound_file_2):
+    sound1 = AudioFileClip(sound_file_1)
+    sound2 = AudioFileClip(sound_file_2)
+
+    return CompositeAudioClip([sound1, sound2.set_start(0)])
+
+
+def save_combined_audio(composite_audio_clip, destination_file):
+    composite_audio_clip.write_audiofile(destination_file, fps =16000)
